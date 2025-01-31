607dbd410cc4bd0a957e2656eda6db0ed7fe8888
1379SamD <1379samd@gmail.com>
2025-01-31 22:28:04 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import Report from './pages/Report.tsx';
+import NoMatch from './pages/NoMatch.tsx';
+import AppLayout from './components/layout/AppLayout.tsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
